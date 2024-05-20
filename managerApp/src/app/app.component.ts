711c018df0d451e043d0356b55542025a6d483bb
unknown <fmmolinero@gmail.com>
2024-05-20 15:54:26 -0300

    probando api

--- managerApp/src/app/app.component.ts
+++ managerApp/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, VERSION } from '@angular/core';
+
+interface FeDeX {
+  IdTipoProducto: number;
+  descripcion: string;
+  created_at: string;
+  updated_at: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'managerApp';
+
+  name = "Angular " + VERSION.full;
+  url = "http://127.0.0.1:8000/api/tipo-producto";
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.callApi();
+  }
+
+  datos: any;
+  callApi() {
+    this.http.get<any>(this.url)
+      .subscribe(
+        (datax: any) => {
+          this.datos = (datax);
+        });
+  }
+
+
+
 }
